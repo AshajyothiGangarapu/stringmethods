ad85f64526aa2aecfe8d2a534e55352f908e830d
ashajyothig <ashajyothi.gangarapu@alacriti.com>
2017-07-13 17:53:05 +0530

    added methods

---
+++ src/main/java/com/alacriti/StringManipulation.java
@@ -0,0 +1,19 @@
+package com.alacriti;
+
+public class StringManipulation {
+	public void concat(String s1,String s2)
+	{
+		String s3=s1.concat(s2);
+		System.out.println(s3);
+	}
+	public void startWith(String s1){
+		boolean y=s1.startsWith("hello");
+		if(y)
+		{
+			System.out.println("entered text starts with hello");
+		}
+		else
+			System.out.println("entered text does not starts with hello");
+	}
+
+}
